770469308e2d15d48e20fc43f5a2de16817c80aa
yxhwxn <officeyh9819@gmail.com>
2024-07-10 01:08:02 +0900

    Init project

---
+++ src/main/java/com/cmc/suppin/SuppinApplication.java
@@ -0,0 +1,13 @@
+package com.cmc.suppin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmc/suppin/SuppinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.suppin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
