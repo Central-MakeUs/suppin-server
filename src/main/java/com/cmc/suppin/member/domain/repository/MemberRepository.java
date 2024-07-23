782c5f3d38b7ad0f8dfe939b0d4a4ae2f358433a
yxhwxn <officeyh9819@gmail.com>
2024-07-23 23:21:25 +0900

    Feat: 유저 회원가입 구현

---
+++ src/main/java/com/cmc/suppin/member/domain/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.cmc.suppin.member.domain.repository;
+
+import com.cmc.suppin.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
