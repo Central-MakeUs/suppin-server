0d4d81f52917739a0501b057c4c9b0c4ed340201
yxhwxn <officeyh9819@gmail.com>
2024-07-31 23:34:33 +0900

    Feat: User 관련 Enum 클래스 추가

---
+++ src/main/java/com/cmc/suppin/global/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.cmc.suppin.global.enums;
+
+public enum UserRole {
+    ROLE_ADMIN,
+    ROLE_USER
+}
---
+++ src/main/java/com/cmc/suppin/global/enums/UserStatus.java
@@ -0,0 +1,5 @@
+package com.cmc.suppin.global.enums;
+
+public enum UserStatus {
+    ACTIVE, INACTIVE, DELETED
+}
