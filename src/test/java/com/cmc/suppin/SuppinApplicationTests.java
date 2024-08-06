58a0cc0cd8d000ba2e3fdb23394a06c75ee7accf
yxhwxn <officeyh9819@gmail.com>
2024-08-06 10:40:39 +0900

    Feat: 쿼리 테스트 코드 추가

--- src/test/java/com/cmc/suppin/SuppinApplicationTests.java
+++ src/test/java/com/cmc/suppin/SuppinApplicationTests.java
@@ -1,13 +1,29 @@
 package com.cmc.suppin;
 
+import com.cmc.suppin.global.enums.UserStatus;
+import com.cmc.suppin.member.domain.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SuppinApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MemberRepository memberRepository;
 
+    @Test
+    public void testExistsByUserIdAndStatusNot() {
+        // Given
+        String userId = "testUser";
+        UserStatus status = UserStatus.DELETED;
+
+        // When
+        boolean exists = memberRepository.existsByUserIdAndStatusNot(userId, status);
+
+        // Then
+        assertThat(exists).isFalse();
+    }
 }
