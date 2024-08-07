743073055928f0e3f9a10d2b9231538bbf5424f8
yxhwxn <officeyh9819@gmail.com>
2024-08-07 22:06:52 +0900

    Feat: 크롤링 데이터 저장 시, datetime으로 변환되어 저장되도록 변경

--- src/main/java/com/cmc/suppin/event/crawl/converter/CommentConverter.java
+++ src/main/java/com/cmc/suppin/event/crawl/converter/CommentConverter.java
@@ -3,15 +3,18 @@ package com.cmc.suppin.event.crawl.converter;
 import com.cmc.suppin.event.crawl.domain.Comment;
 import com.cmc.suppin.event.events.domain.Event;
 
+import java.time.LocalDateTime;
+
 public class CommentConverter {
 
-    public static Comment toCommentEntity(String author, String text, String time, String url, Event event) {
+    public static Comment toCommentEntity(String author, String text, LocalDateTime commentDate, String url, Event event) {
         return Comment.builder()
                 .author(author)
                 .commentText(text)
-                .commentDate(time)
+                .commentDate(commentDate)
                 .url(url)
                 .event(event)
                 .build();
     }
 }
+
