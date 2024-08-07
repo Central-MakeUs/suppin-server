407e05b5fa62d44b5dd22e4c046d55af712a25f6
yxhwxn <officeyh9819@gmail.com>
2024-08-07 22:54:56 +0900

    Chore: BaseDateTimeEntity 적용

--- src/main/java/com/cmc/suppin/SuppinApplication.java
+++ src/main/java/com/cmc/suppin/SuppinApplication.java
@@ -2,12 +2,14 @@ package com.cmc.suppin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SuppinApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SuppinApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SuppinApplication.class, args);
+    }
 
 }
